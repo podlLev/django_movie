f0c96e932e02aa1e77a3f6e4baf9af4076264c84
podlLev <pidlisnyilev@gmail.com>
2023-01-04 13:10:23 +0200

    Creating and editing classes for the admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -2,11 +2,85 @@ from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('id', 'name')
+
+
+class ReviewInline(admin.StackedInline):
+    """Инлайн для отзывов  на странице фильма"""
+    model = Reviews
+    extra = 1
+    fields = ('name', 'email', 'text', 'parent')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'), )
+        }),
+        (None, {
+            'fields': ('description', 'poster')
+        }),
+        (None, {
+            'fields': (('year', 'world_premiere', 'country'), )
+        }),
+        ('Actors', {
+            'classes': ('collapse', ),
+            'fields': (('actors', 'directors', 'genres', 'category'), )
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'), )
+        }),
+        ('Options', {
+            'fields': (('url', 'draft'), )
+        }),
+    )
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    fields = ('name', 'email', 'text', 'parent', 'movie')
+    list_display = ('id', 'name', 'email', 'parent', 'movie')
+    list_display_links = ('id', 'name')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ('name', 'url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Акторы"""
+    list_display = ('name', 'age')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ('star', 'ip')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ('title', 'movie')
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
